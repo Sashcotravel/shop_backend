f906dc3ff986ab285ef875232c8915e99e44569a
Sashcotravel <vasinoleksandr1@gmail.com>
2023-03-16 22:26:25 +0200

    26

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ mongoose.connect('mongodb+srv://SashkoTravel:gowno444@cluster0.xqc027t.mongodb.n
 
 const app = express()
 app.use(express.json())
+app.use(cors({
+    origin: '*'
+}));
 app.use(compression())
 
 app.use(express.static('build'))
